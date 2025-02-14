9bb7946cf17911465c80884ffaa1b1a840c0b1df
VictorianHues <VictorianHues@gmail.com>
2025-02-14 11:44:06 +0100

    Class Implementation of Vibrating String

---
+++ src/main_vibr_string.py
@@ -0,0 +1,74 @@
+import numpy as np
+import math
+
+from vibr_string import VibratingString
+
+
+def main():
+    length = 1.0
+    spatial_points = 101
+    total_time = 1.0
+    time_step_size = 0.001
+    c = 1.0
+
+    sine_constant = 2
+    
+
+    string = VibratingString(length, 
+                             spatial_points, 
+                             total_time, 
+                             time_step_size, 
+                             c, 
+                             lambda x: np.sin(sine_constant * np.pi * x))
+
+    string.solve()
+    string.plot_heat_map()
+    string.plot_animation()
+
+    mean = 0.5
+    stdDev = 0.1
+    
+    string = VibratingString(length, 
+                             spatial_points, 
+                             total_time, 
+                             time_step_size, 
+                             c, 
+                             lambda x: np.exp(-((x - mean)**2) / (2 * stdDev**2)))
+
+    string.solve()
+    string.plot_heat_map()
+    string.plot_animation()
+
+
+
+    sine_constant = 5
+
+    string = VibratingString(length, 
+                             spatial_points, 
+                             total_time, 
+                             time_step_size, 
+                             c, 
+                             lambda x: np.sin(sine_constant * np.pi * x))
+
+    string.solve()
+    string.plot_heat_map()
+    string.plot_animation()
+
+    mask_start = 1/5
+    mask_end = 2/5
+
+    string = VibratingString(length, 
+                             spatial_points, 
+                             total_time, 
+                             time_step_size, 
+                             c, 
+                             lambda x: np.sin(sine_constant * np.pi * x),
+                             mask_start,
+                             mask_end)
+
+    string.solve()
+    string.plot_heat_map()
+    string.plot_animation()
+
+if __name__ == "__main__":
+    main()
--- src/vibr_string.py
+++ src/vibr_string.py
@@ -1,53 +1,109 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-c = 1.0
-length = 1.0
-spatial_points = 101
-spatial_step = length / (spatial_points - 1)
-time_step = 0.001
-total_time = 10.0
+from typing import Optional
+
+class VibratingString:
+    def __init__(self, 
+                 length : float, 
+                 spatial_points : int, 
+                 total_time : float, 
+                 time_step_size : float, 
+                 c : float, 
+                 initial_condition_func : callable, 
+                 mask_start : Optional[float] = None, 
+                 mask_end : Optional[float] = None):
+        self.length = length
+        self.spatial_points = spatial_points
+        self.spatial_step_size = length / (spatial_points - 1)
+        self.total_time = total_time
+        self.time_step_size = time_step_size
+        self.c = c
+        self.initial_condition_func = initial_condition_func
+
+        self.x_points = np.linspace(0, length, spatial_points)
+
+        self.time_steps = int(total_time / time_step_size)
+
+        # Create a mask to apply the initial condition only to a portion of the string
+        if mask_start is None:
+            mask_start = 0
+        if mask_end is None:
+            mask_end = self.length
+        self.mask = (self.x_points > mask_start) & (self.x_points < mask_end)
+
+        self.__initialize_psi_grid()
 
 
-x = np.linspace(0, length, spatial_points)
+    def __initialize_psi_grid(self):
+        self.Psi_grid = np.zeros((self.time_steps, self.spatial_points))
 
-time_steps = int(total_time / time_step)
+        for i in range(self.spatial_points):
+            if self.mask[i]:
+                self.Psi_grid[0, i] = self.initial_condition_func(self.x_points[i])
 
-Psi = np.exp(-((x - length/2)**2) / 0.1)
-
-Psi_grid = np.zeros((time_steps, spatial_points))
-
-Psi_prev = Psi.copy()  
-Psi_next = np.zeros(spatial_points) 
-
-
-plt.ion()
-fig, ax = plt.subplots()
-line, = ax.plot(x, Psi)
-ax.set_ylim(-1.2, 1.2)
-ax.set_xlim(0, length)
+        # Set Boundary Conditions
+        self.Psi_grid[:, 0] = 0
+        self.Psi_grid[:, -1] = 0
 
 
+    # Private method to initialize the first time step
+    def __initialize_first_time_step(self):
+        for i in range(1, self.spatial_points - 1):
+            self.Psi_grid[1, i] = (self.Psi_grid[0, i] +
+                                0.5 * (self.c**2 * self.time_step_size**2 / self.spatial_step_size**2) *
+                                (self.Psi_grid[0, i+1] - 2*self.Psi_grid[0, i] + self.Psi_grid[0, i-1]))
 
-for n in range(time_steps):
-    for i in range(1,spatial_points-1):
-        Psi_next[i] = c**2 * time_step**2 / spatial_step**2 * (Psi[i+1] - 2*Psi[i] + Psi[i-1]) + 2*Psi[i] - Psi_prev[i]
-    
-    Psi_prev = Psi.copy()
-    Psi = Psi_next.copy()
+    # Private method to update the grid
+    def __update_grid(self, n, i):
+        self.Psi_grid[n+1, i] = (self.c**2 * self.time_step_size**2 / self.spatial_step_size**2 * 
+                                (self.Psi_grid[n, i+1] - 2*self.Psi_grid[n, i] + self.Psi_grid[n, i-1])
+                                + 2*self.Psi_grid[n, i] - self.Psi_grid[n-1, i])
 
-    Psi_grid[n, :] = Psi
+    def solve(self):
+        self.__initialize_first_time_step()
 
-    if n % 10 == 0:
-        line.set_ydata(Psi)
-        plt.pause(0.01)
+        for n in range(1, self.time_steps - 1):
+            for i in range(1, self.spatial_points - 1):
+                self.__update_grid(n, i)
+
+        self.Psi_grid[:, 0] = 0
+        self.Psi_grid[:, -1] = 0
 
 
-plt.ioff()
-plt.show()
+    def plot_heat_map(self):
+        plt.figure()
+        plt.imshow(self.Psi_grid, aspect='auto', cmap='hot', extent=[0, self.length, self.total_time, 0])
+        plt.colorbar(label='Psi')
+
+        plt.xlabel('X')
+        plt.ylabel('Time')
+        plt.title('Vibrating String')
+
+        plt.show()
 
 
-plt.figure()
-plt.imshow(Psi_grid, aspect='auto', cmap='hot', extent=[0, length, total_time, 0])
-plt.colorbar(label='Psi')
-plt.show()
+    def plot_animation(self):
+        plt.ion()
+        fig, ax = plt.subplots()
+        line, = ax.plot(self.x_points, self.Psi_grid[0])
+        ax.set_ylim(-1, 1)
+        ax.set_xlim(0, self.length)
+
+        self.close = False
+
+        def close_event(event):
+            self.close = True
+
+        fig.canvas.mpl_connect('close_event', close_event)
+
+        for n in range(self.time_steps-1):
+            if self.close:
+                break
+
+            line.set_ydata(self.Psi_grid[n])
+            fig.canvas.draw()
+            plt.pause(0.01)
+
+        plt.ioff()
+        plt.show()
