ef388f42cdd722604e85961b6b851a32110d6a9d
PaulJungnickel <jungnikp@gmail.com>
2025-02-16 15:35:06 +0100

    benchmark cpp vs njit

--- src/jacobi_iteration.py
+++ src/jacobi_iteration.py
@@ -8,7 +8,7 @@ class Jacobi:
         self.epsilon = epsilon
         self.max_iter = max_iter
         
-        reset_state(self)
+        self.reset_state()
 
     def solve(self):
         
@@ -39,48 +39,49 @@ class Jacobi:
 
         return self.c_new, self.max_iter
     
-def reset_state(self):
-    
-    # Initialize grid (two matrices c_old and c_new)
-    self.c_new = np.zeros((self.n_steps, self.n_steps))
-    self.c_new[:, -1] = 1.0 
-    self.c_new[:, 0] = 0.0   
-    self.c_old = self.c_new.copy()
+    def reset_state(self):
+        
+        # Initialize grid (two matrices c_old and c_new)
+        self.c_new = np.zeros((self.n_steps, self.n_steps))
+        self.c_new[:, -1] = 1.0 
+        self.c_new[:, 0] = 0.0   
+        self.c_old = self.c_new.copy()
 
 
-def bench_functions(self):
+    def bench_functions(self):
 
-    t = 0
-    num_runs = 10
-    self.max_iter = 10000
-    max_iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
-    for i in range(num_runs):
-        reset_state(self)
-        start = time.time()
-        iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
-        t += time.time() - start
-        if(iter != max_iter):
-            print(iter, max_iter)
-    print(t)
-            
-    t = 0
-    for i in range(num_runs):
-        reset_state(self)
-        start = time.time()
-        iter = ffd.fast_jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
-        t += time.time() - start
-        if(iter != max_iter):
-            print(iter, max_iter)
+        t = 0
+        num_runs = 10
+        self.max_iter = 100000
+        max_iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
+        print(max_iter, ' iterations')
+        for i in range(num_runs):
+            self.reset_state
+            start = time.time()
+            iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
+            t += time.time() - start
+            if(iter != max_iter):
+                print(iter, max_iter)
+        print(t)
+                
+        t = 0
+        for i in range(num_runs):
+            self.reset_state()
+            start = time.time()
+            iter = ffd.fast_jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
+            t += time.time() - start
+            if(iter != max_iter):
+                print(iter, max_iter)
 
-    print(t)
+        print(t)
 
 
 
     
 # Example usage
 if __name__ == '__main__':
-    solver = Jacobi(50)
-    bench_functions(solver)
+    solver = Jacobi(200)
+    solver.bench_functions()
     # c_new, iteration = solver.solve()
     # print(f"Converged after {iteration} iterations")
     # print(c_new)
