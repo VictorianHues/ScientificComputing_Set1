5652b779222089e06cf24236070ccf83c8f0d364
PaulJungnickel <jungnikp@gmail.com>
2025-02-16 15:20:35 +0100

    small fix of jacobi

--- src/jacobi_iteration.py
+++ src/jacobi_iteration.py
@@ -1,18 +1,17 @@
 import numpy as np
 from finite_difference import jacobi
+from cpp_modules import fast_finite_difference as ffd
+import time
 class Jacobi:
     def __init__(self, n_steps, epsilon=1e-5, max_iter=100000):
         self.n_steps = n_steps
         self.epsilon = epsilon
         self.max_iter = max_iter
         
-        # Initialize grid (two matrices c_old and c_new)
-        self.c_new = np.zeros((n_steps, n_steps))
-        self.c_new[:, -1] = 1.0 
-        self.c_new[:, 0] = 0.0   
-        self.c_old = self.c_new.copy()
+        reset_state(self)
 
     def solve(self):
+        
         self.max_iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
         # for iteration in range(self.max_iter):
             
@@ -39,10 +38,49 @@ class Jacobi:
         #     np.copyto(self.c_old, self.c_new) 
 
         return self.c_new, self.max_iter
+    
+def reset_state(self):
+    
+    # Initialize grid (two matrices c_old and c_new)
+    self.c_new = np.zeros((self.n_steps, self.n_steps))
+    self.c_new[:, -1] = 1.0 
+    self.c_new[:, 0] = 0.0   
+    self.c_old = self.c_new.copy()
 
+
+def bench_functions(self):
+
+    t = 0
+    num_runs = 10
+    self.max_iter = 10000
+    max_iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
+    for i in range(num_runs):
+        reset_state(self)
+        start = time.time()
+        iter = jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
+        t += time.time() - start
+        if(iter != max_iter):
+            print(iter, max_iter)
+    print(t)
+            
+    t = 0
+    for i in range(num_runs):
+        reset_state(self)
+        start = time.time()
+        iter = ffd.fast_jacobi(self.c_old, self.c_new, self.max_iter, self.n_steps, self.epsilon)
+        t += time.time() - start
+        if(iter != max_iter):
+            print(iter, max_iter)
+
+    print(t)
+
+
+
+    
 # Example usage
 if __name__ == '__main__':
     solver = Jacobi(50)
-    c_new, iteration = solver.solve()
-    print(f"Converged after {iteration} iterations")
-    print(c_new)
+    bench_functions(solver)
+    # c_new, iteration = solver.solve()
+    # print(f"Converged after {iteration} iterations")
+    # print(c_new)
